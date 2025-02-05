fba3e3db0e3ff436ff3ddb36e127a259f7271116
J-P-Zara <jzara792@cougarmail.cccc.edu>
2025-02-04 19:07:08 -0500

    Add files via upload

--- eclipse-workspace/ch2inclass1/src/Eggs.java
+++ eclipse-workspace/ch2inclass1/src/Eggs.java
@@ -1,12 +1,5 @@
 //Jessie Zarate 1/29/2025 CSC 151 ch 2 assignment 1
-/*
- * Meadowdale Dairy Farm sells organic brown eggs to local customers. 
- * It charges $3.25 for a dozen eggs, or 45 cents for individual eggs that are not part of a dozen. 
- * Write a program that prompts a user for the number of eggs in the order and then display the amount
- * owed with a full explanation using the following wording: You ordered 27 eggs. 
- * That’s 2 dozen at $3.25 per dozen and 3 loose eggs at 45 cents each for a total of $7.85.  
- * You can name the class Eggs.java
- */
+
 
 import java.util.Scanner;
 
@@ -23,12 +16,13 @@ public class Eggs {
 		
 		int dozen = eggs / EGGS_IN_DOZEN;
 		int remainder = eggs % EGGS_IN_DOZEN;
+		
 		final double PRICE_OF_INDIVIDUAL_EGG=0.45;
 		final double PRICE_FOR_DOZEN=3.25;
 		
 		double singlePrice = remainder * PRICE_OF_INDIVIDUAL_EGG;
-		double DozenPrice = dozen * PRICE_FOR_DOZEN;
-		double sum = DozenPrice + singlePrice;
+		double dozenPrice = dozen * PRICE_FOR_DOZEN;
+		double sum = dozenPrice + singlePrice;
 		
 		System.out.println("You ordered " + eggs + " eggs. That's " + dozen + " dozen "
 		+ "at $3.25 per dozen and " + remainder + " loose eggs at 45 cents each for a total "
